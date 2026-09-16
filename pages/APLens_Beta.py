f5d6d66e7e8f4fb6519c22fc3b68eba503fa9897
Arun Peswani <arunpeswani@gmail.com>
2026-09-16 08:29:01 +0530

    Update APLens_Beta.py

--- pages/APLens_Beta.py
+++ pages/APLens_Beta.py
@@ -177,8 +177,7 @@ with header_col2:
                 st.caption("Authenticate instantly via Supabase OAuth providers:")
                 try:
                     project_url = st.secrets["SUPABASE_URL"]
-                    google_url = f"{project_url}/auth/v1/authorize?provider=google"
-                    github_url = f"{project_url}/auth/v1/authorize?provider=github"
+                    google_url = f"{project_url}/auth/v1/authorize?provider=google&prompt=select_account"
                     
                     st.markdown(f"""
                         <div class="login-container">
